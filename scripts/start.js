98bd51e1382453820406205304f375e95aa8437a
mariofdezzz <marioferrerofdez@gmail.com>
2020-12-10 19:44:54 +0000

    🎉 Initial commit

---
+++ scripts/start.js
@@ -0,0 +1,23 @@
+const os = require('os');
+const { spawn } = require("child_process")
+const { readdirSync } = require("fs")
+const { cp, mkdir } = require("shelljs")
+
+const day = process.argv[2]
+const years = readdirSync("./src")
+
+if (!years.includes("2020")) {
+  mkdir("src/2020");
+}
+const days = readdirSync("./src/2020")
+
+if (!days.includes(day)) {
+  console.log(`Creating file structure for ${day}...`)
+  cp("-r", "src/template", `src/2020/${day}`)
+}
+
+const nodemonExecutablePath = os.platform() === "win32" ? "node_modules\\.bin\\nodemon.cmd" : "nodemon";
+
+spawn(nodemonExecutablePath, [`src/2020/${day}/index.js`], {
+  stdio: "inherit",
+})
---
+++ src/template/index.js
@@ -0,0 +1,22 @@
+const { parseData } = require("../../utils")
+
+const data = parseData(__dirname)
+
+const part1 = (input) => {
+  return
+}
+
+const part2 = (input) => {
+  return
+}
+
+
+/* Results */
+
+console.time("Time")
+const result1 = part1(data.input)
+const result2 = part2(data.input)
+console.timeEnd("Time")
+
+console.log("Solution to part 1:", result1)
+console.log("Solution to part 2:", result2)
