d18f4517387170c8111689963bf39173bcc79ea4
mariofdezzz <marioferrerofdez@gmail.com>
2020-12-10 21:13:26 +0000

    💥 Build script
    Generates README

---
+++ scripts/build.js
@@ -0,0 +1,35 @@
+const { loader } = require("../src/utils")
+const fs = require("fs")
+
+const achieved = JSON.parse(fs.readFileSync("src/achieved.json", "utf8"))
+
+let years = ""
+for (const year in achieved) {
+    let table = ""
+    let completed = 0
+
+    for (let day = 1; day < 25; day++) {
+        let stars = 0
+        if (achieved[year].hasOwnProperty(day))
+            stars = achieved[year][day]
+
+        completed += stars
+        table += `|**${day}: ` + "🌟".repeat(stars) + "**"
+        if (day % 6 == 0) table += "|\n"
+        if (day == 6) table += "|---".repeat(6) + "|\n"
+    }
+
+    years += `## ${year} \n\n🌟 ${completed}/50 \n\n${table} \n`
+}
+
+let text = `
+# 🌠 Advent Of Code 
+🎄 Christmas hobby made with JavaScript
+
+${years}`
+
+console.time("README created in")
+fs.writeFile("README.md", text, (err) => {
+    if (err) throw err
+    console.timeEnd("README created in")
+})
